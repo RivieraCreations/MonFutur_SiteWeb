d8e05f8104a3bc8d1e931413d0657bbad8d651b3
RivieraCreations <fccreation@icloud.com>
2015-04-20 19:42:06 +0200

    Ajout du fichier configuration.php et des ses infos

---
+++ configuration.php
@@ -0,0 +1,8 @@
+
+Fichier de configuration du site.
+---------------------------------
+
+Il faudra utiliser ces codes pour la création du site 
+Avec l'un des frameworks suivants : 
+CodeIgniter / Laravel ou Silex
+
