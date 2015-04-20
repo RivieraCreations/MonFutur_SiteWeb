02529410298f0b034a41fb9d1eef6b1a458ff344
RivieraCreations <fccreation@icloud.com>
2015-04-20 19:47:30 +0200

    Ajout du fichier index.php comme base pour le projet

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Riviéra Créations : Création de site Internet en 4 étapes clés sur Cagnes sur mer.</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Bootstrap.min.css pour Production -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap.css pour Developpement -->
+        <!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
+        <!-- Styles CSS Persos -->
+        <link rel="stylesheet" href="css/style.min.css" type="text/css">
+        
+        <meta name="description" content="Créations de sites Internet en 4 étapes clés sur le 06 par un webmaster/développeur web passionné pour vous aider à la réussite de votre projet.">
+        <meta name="Keywords" content="riviera, creation, creation site web, developpement, web, e-marketing, référencement, php, mysql, html5, css3, javascript, ajax, jquery, framework, codeigniter, cakephp, laravel, 960gs, referencement, seo, pao, multimedia." />
+        <meta name="author" content="rivieracreations">
+        <link rel="shortcut icon" href="ico/favicon.png">
+
+        <meta name="google-site-verification" content="zpq_TT8n73nZOvl7DWWxPBYL6w-TUasWTAfW0Z-aQm0" />
+        
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head> 
+    <body>
+    <!-- Pour gérer les Scrolls avec jQuery -->
+    <a name="top"></a>
+    <div id="header"></div>
+        <!-- ****** Suivi Google Analytics -->
+        <?php include_once("inc/analyticstracking.php");?>
+            
+        <!-- ========== NAVBAR - Barre Navigation centré sur les 12 colonnes -->
+        <div class="head-nav">
+            <div class="container"><!-- Container 1 -->
+                <!-- Menu header -->
+                <header class="nav-bar navbar-static-top bs-docs-nav white" role="banner">
+                    <div class="container"><!-- Container 2 -->
+                        <div class="navbar-header">
+                            <!-- bouton dépliement du menu -->
+                            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                                <span class="sr-only">Navigation mobile</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                            </button><!-- end Toogle -->
+
+                            <!-- LOGO -->
+                            <a href="http://www.riviera-creations.com" class="navbar-brand">
+                                <img src="img/header/logo-riviera-creations.png" class="img-responsive center-left" alt="logo riviera creations" />
+                            </a><!-- end LOGO -->
+                        </div><!-- end navbar-header -->
+
+                        <!-- ******** Menu navigation principal ********-->
+                        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="active"><a href="accueil.php">Accueil</a></li>
+                                <li><a href="creation-de-site-web.php">Activités</a></li>
+                                <li><a href="portfolio.php">Portfolio</a></li>
+                                <li><a href="competences.php">Compétences</a></li>
+                                <li><a href="sources.php">Sources</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                                <!-- bouton Inscription -->
+                                <!-- <li>
+                                    <a class="btn" href="#"><button type="button" class="btn btn-lg btn-primary">S'inscrire</button></a>
+                                </li> -->
+                            </ul><!-- end UL -->
+                        </nav><!-- end nav-bar-header -->                      
+                        <!-- SLOGAN Riviera Creations -->
+                        <div class="sloganRC">
+                            <em>La passion de proposer des solutions <br /> .. à valeurs ajoutées !</em>
+                        </div><!-- end SLOGAN Riviera Creations -->
+                    </div><!-- end container 2 -->
+                </header><!-- end header -->
+            </div><!-- end container 1 -->
+        </div><!-- END HEAD NAVBAR -->
+
+        <!-- ========== Block SLOGAN centrée sur les 12 colonnes -->
+        <div class="container">
+            <div class="row">
+                <!-- ==========  Block SLOGAN ********* -->
+                <div class="col-lg-12">
+                    <h1 class="blue text-center"><strong>Création de site Internet en 4 étapes clés</strong></h1>
+                <br />
+                </div><!-- end rcol-lg-12 -->
+            </div><!-- end row -->
+        </div><!-- end Container -->
+
+        <!-- ********************************* -->
+        <!--         include du Carousel       -->
+        <!-- ********************************* -->
+        <?php ?>
+        
+
+        <br /><br />
+        <!-- ==========  Block CONTAINER : 4 CERCLES Thématiques ********* -->
+        <div class="head grey">
+            <div class="container">
+                <h2 class="pink text-center"><strong>Réussir votre site web vitrine ou e-commerce
+                    <br /><br />avec un Chef de projet web passionné !</strong></h2>
+                <br />
+                <div class="row">
+                    <div class="col-sm-3"><!-- Colonne 1 -->
+                        <h3 class="text-center"><strong>Stratégie</strong></h3>
+                            <br />
+                            <a href="creation-de-site-web.php#strategie" title="Définir et mettre en oeuvre la stratégie correspondante pour atteindre les objectifs du projet.">
+                                <img src="img/creation-de-site-web/cercle-strategie.png" class="img-responsive img-circle center-block" alt="cercle graphique la stratégie web" />
+                            </a>
+                            <br />
+                            <p class="black text-center">
+                                Définir et mettre en oeuvre la stratégie correspondante pour atteindre les objectifs du projet.
+                            </p>
+                    </div><!-- END col-sm-3 -->
+                    
+                    <div class="col-sm-3"><!-- Colonne 2 -->
+                        <h3 class="text-center"><strong>Design</strong></h3>
+                            <br />
+                            <a href="creation-de-site-web.php#webdesign" title="Concevoir la charte graphique, design du site, d'après le prototypage et effectuer son intégration { HTML Responsive } Mobile first oblige !">
+                                <img src="img/creation-de-site-web/cercle-webdesign.png" class="img-responsive img-circle center-block" alt="cercle graphique le Design web" />
+                            </a>
+                            <br />
+                            <p class="black text-center">
+                                Concevoir la charte graphique, design du site, d'après le prototypage et effectuer son intégration { HTML Responsive } Mobile first oblige !
+                            </p>
+                    </div><!-- END col-sm-3 -->                        
+                    
+                    <div class="col-sm-3"><!-- Colonne 3 -->
+                        <h3 class="text-center"><strong>Codage</strong></h3>
+                            <br />
+                            <a href="creation-de-site-web.php#developpement" title="Le Codage informatique pur de l'application web ou du module à concevoir avec un Framework ou CMS ou sans (à la main).">
+                                <img src="img/creation-de-site-web/cercle-developpement.png" class="img-responsive img-circle center-block" alt="cercle graphique Développement web" />
+                            </a>
+                            <br />
+                            <p class="black text-center">
+                                Le Codage c'est le développement informatique pur de l'application web ou du module à concevoir avec un Framework ou CMS ou sans (à la main). 
+                            </p>
+                    </div><!-- END col-sm-3 -->                        
+                    
+                    <div class="col-sm-3"><!-- Colonne 4 -->
+                        <h3 class="text-center"><strong>Production</strong></h3>
+                            <br />
+                            <a href="creation-de-site-web.php#production" title="Optimisation du référencement mots clés, titres, contenus, Standards du W3C, Services e-Marketing">
+                                <img src="img/creation-de-site-web/cercle-production.png" class="img-responsive img-circle center-block" alt="cercle graphique la production web" />
+                            </a>
+                            <br />
+                            <p class="black text-center">
+                                Optimisation du référencement mots clés, titres, contenus, Standards du W3C, Services e-Marketing
+                            </p>
+                    </div><!-- END col-sm-3 -->
+                </div><!-- END row -->
+            <div>
+                <img src="img/site/tothetop-gris.png" alt="remonter haut de page"/>
+                <a class="gris" href="#header">Remonter</a>
+            </div>
+            <br />
+            </div><!-- END container -->
+        </div><!-- END head --> 
+        <!-- END Block CONTAINER : 4 CERCLES Thématiques ********* -->
+        <br />
+
+        <!-- ********* Block COMMENT s'y prendre centré sur les 12 colonnes -->
+        <div class="container">
+            <div class="row">
+                <h2 class="blue text-center"><strong>Comment s'y prendre, s'organiser, par où et quoi commencer ?</strong></h2>
+                <h2 class="pink text-center"><strong></strong>Quel est le meilleur outils de création pour un blog, un site vitrine ou e-commerce ?</h2>
+                <h2 class="blue text-center"><strong></strong>Quel C.M.S choisir et pour quels usages ? <br /> Wordpress, Joomla, Drupal, Prestashop, etc.. </h2>
+                <h2 class="pink text-center"><strong>Un Framework PHP plutôt qu'un C.M.S ? quels avantages ? <br /> SPIP, CodeIgniter, Laravel, CakePHP, Symfony ou Zend Framework, etc..</strong></h2>
+                <h2 class="blue text-center"><strong>Quel coût et délais prévoir pour la réussite de votre site et qu'il soit en ligne ?</strong></h2>
+                <h2 class="pink text-center"><strong>Comment être sur la première page de Google ?</strong></h2>
+                <h2 class="blue text-center"><strong>Qu'est-ce que l'e-marketing ? A quoi ça sert ?</strong></h2>
+                <h2 class="pink text-center"><strong>Les réseaux sociaux servent-ils vraiment à populariser mon site web ?</strong></h2>
+            </div><!-- end row -->
+        </div><!-- end Container -->
+        <!-- ********* END Block COMMENT s'y prendresur 12 colonnes -->
+        <br />
+
+        <!-- ********* Block CONTAINER 1 - Texte WEBMASTER + l'image ********* -->
+        <div class="head-home projets">
+            <div class="container"><!-- Block 1 partie de gauche -->
+                <div class="row">
+                    <!-- ======== Block 1 partie de gauche sur 5 colonnes ======== -->
+                    <div class="col-md-5">
+                        <div class="page-header text-right">
+                            <h3 class="webmaster">
+                                <strong>Un chef de projet web vous accompagne pour la réussite de votre site web vitrine, e-commerce ou simple blog <br />en 4 étapes clés !</strong><br />
+                            </h3>
+                            <p class="black text-right">
+                                <em>(Stratégie e-Marketing, Web Design, Développement, Référencement SEO)</em>
+                            </p>
+                        </div><!-- end page-header -->
+                        <p class="description lead text-right">
+                            Webmaster-Développeur passionné depuis plus de 6 ans, j'accompagne les entrepreneurs et entreprises en proposant des solutions web globales pour réussir leur présence et leur communication numérique.
+                            <br /><br />
+                            Un site web réussi devient alors un outil incontournable vous apportant de réelles opportunités d'affaires nouvelles ! 
+                            <br /><br />
+                            Que ce soit pour trouver de nouveaux clients et les fidéliser de façon efficace, de nouveaux partenaires, il fédère votre marque, votre notoriété, fait connnaître la qualité de vos produits et services et accroît votre chiffre d'affaires ! 
+                        </p><!-- end Lead-->
+                    </div><!-- end col-md-5-->
+                    <br /><br />
+                    <!-- ======== Block 2 partie de droite sur 7 colonnes ======== -->
+                    <div class="col-md-7">
+    
+                        <a href="competences.php">
+                            <img src="img/accueil/ecran-webmaster.jpg" class="img-responsive center-block" alt="écran webmaster" title="Voir toutes mes compétences : webmaster-développeur web PHP5 / e-Marketing" />
+                        </a>
+                            <br />
+                        <a href="creation-de-site-web.php">
+                            <img src="img/accueil/les-4-etapes-cles.jpg" class="img-responsive center-block" alt="4 étapes clés" title="Les 4 étapes clés détaillées d'un site web de qualité pro" />
+                        </a>
+                        <br /><br />
+                    </div><!-- END col-md-7 -->
+                </div><!-- end row ligne 1 -->
+            <div>
+                <img src="img/site/tothetop-gris.png" alt="remonter haut de page"/>
+                <a class="gris" href="#header">Remonter</a>
+            </div>
+            </div><!-- end Container -->
+            <!-- END Block contenu : ligne 1 séparée en 2 parties -->
+        </div><!-- end head-home -->
+    <hr />
+        
+    <!-- ****** INCLUSION du FOOTER -->
+    <?php include('inc/footer.php');?>
